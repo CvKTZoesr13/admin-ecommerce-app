44fd6f9e027e5ee870b5a3d3c4ddea822a32295c
CvKTZoesr13 <vanducdo4@gmail.com>
2023-07-24 23:54:48 +0700

    create brand

--- src/features/brand/brandService.js
+++ src/features/brand/brandService.js
@@ -1,13 +1,19 @@
 import axios from "axios";
 import { BASE_URL } from "../../utils/base_url";
-
+import { config } from "../../utils/axiosConfigToken";
 const getBrands = async () => {
   const response = await axios.get(`${BASE_URL}brand/`);
   return response.data;
 };
 
+const createBrand = async (brand) => {
+  const response = await axios.post(`${BASE_URL}brand/`, brand, config);
+  return response.data;
+};
+
 const brandService = {
   getBrands,
+  createBrand,
 };
 
 export default brandService;
--- src/pages/AddBrand.jsx
+++ src/pages/AddBrand.jsx
@@ -1,16 +1,77 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CustomInput from "../components/CustomInput";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { createBrand } from "../features/brand/brandSlice";
 
+let schema = Yup.object().shape({
+  title: Yup.string().required("Vui lòng nhập tên thương hiệu."),
+});
 const AddBrand = () => {
+  const dispatch = useDispatch();
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+    },
+    validationSchema: schema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+      dispatch(createBrand(values));
+      formik.resetForm();
+      setTimeout(() => {
+        // todo
+        navigate("/admin/list-brand");
+      }, 3000);
+    },
+  });
+  const navigate = useNavigate();
+  const newBrand = useSelector((state) => state.brands);
+  const { isSuccess, isLoading, isError, createdBrand } = newBrand;
+  useEffect(() => {
+    if (isSuccess && createdBrand !== "") {
+      toast("Thêm thương hiệu mới thành công!", {
+        position: "top-right",
+        autoClose: 2500,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+    if (isError) {
+      toast("Có lỗi xảy ra!", {
+        position: "top-right",
+        autoClose: 2500,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  }, [isSuccess, isLoading, isError, createdBrand]);
   return (
     <div>
       <h3 className="mb-4 title">Thêm thương hiệu sản phẩm</h3>
       <div>
-        <form action="">
+        <form action="" onSubmit={formik.handleSubmit}>
           <CustomInput
             type="text"
             placeholder="Nhập thương hiệu sản phẩm mới"
+            name="title"
+            onChange={formik.handleChange("title")}
+            onBlur={formik.handleBlur("title")}
+            value={formik.values.title}
           />
+          <div className="text-danger">
+            {formik.touched.title && formik.errors.title}
+          </div>
           <button
             type="submit"
             className="btn btn-success border-0 rounded-3 mt-3 float-end"
